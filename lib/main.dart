3ab9d04fa414ec341184da8ee144e537ce1677da
saaibyyr <112531219+saaibyyr@users.noreply.github.com>
2023-09-10 19:27:13 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text(
-          "Hello")
+      home: Scaffold(appBar: AppBar(backgroundColor: Colors.amberAccent,),
+      body: Container(color: Colors.pink, child: Text("Saibyr Aierke")),)
     );
   }
 }
